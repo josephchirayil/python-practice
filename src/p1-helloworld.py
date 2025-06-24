c138007bfd804db6556f85384a71ca85d026237e
josephchirayil <jonah.chirayil@gmail.com>
2025-06-24 14:41:02 -0400

    Update p1-helloworld.py
    Spelling mistake fix

--- src/p1-helloworld.py
+++ src/p1-helloworld.py
@@ -8,6 +8,6 @@ print('The length of your name is:')
 print(len(myName))
 
 # Asks for age
-primt('What is your age?')
+print('What is your age?')
 myAge = input()
 print('You will be' + str(int(myAge) + 1) + 'in a year.')
