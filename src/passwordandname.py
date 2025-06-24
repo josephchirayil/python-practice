10e726b10249694d13b7b1c377705251c59857e0
josephchirayil <jonah.chirayil@gmail.com>
2025-06-24 15:00:44 -0400

    Update passwordandname.py indentations

--- src/passwordandname.py
+++ src/passwordandname.py
@@ -1,12 +1,11 @@
 while True:
-    print('Who are you?')
-    name = input()
-    if name != 'Jonah':
-        continue
+   print('Who are you?')
+   name = input()
+   if name != 'Jonah':
+      continue
    print('Hello,' + name + 'What is the password? (It is a horrible password.')
    password = input()
-    if password == 'password':
-         break
-print('Access granted.')
-        
-    
+   if password == 'password':
+      break
+   print('Access granted.')
+   
